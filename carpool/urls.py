8db97d2981be47fad1523506e3317ae4f5af9b04
mugala <mugalalevi21@gmail.com>
2018-05-18 12:54:06 +0300

    Urls for project This commit will build the url for the home page for project

--- carpool/urls.py
+++ carpool/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^$', views.welcome, name = "welcome"),
     url(r'', include ("drivers.urls")),
     url(r'', include ("riders.urls")),
 ]
