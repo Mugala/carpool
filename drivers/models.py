da02faac8f1b271b13286b4c2c516e4ab4b8edaa
mugala <mugalalevi21@gmail.com>
2018-05-20 17:55:04 +0300

    Models This commit will create the models for both the driver and the rider apps

--- drivers/models.py
+++ drivers/models.py
@@ -4,16 +4,30 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class tags(models.Model):
+class Drivertag(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
 
+class Car (models.Model):
+    Car_model = models.CharField(max_length = 30)
+    number_plate = models.CharField(max_length = 30)
+    number_seats = models.IntegerField()
+
+class Pickup_location(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
 class Driver (models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
-    pickup_point = models.CharField(max_length = 50)
-    tags = models.ManyToManyField(tags)
+    pickup_point = models.ForeignKey(Pickup_location)
+    destination = models.CharField(max_length = 50)
+    tags = models.ManyToManyField(Drivertag)
+    car = models.ForeignKey(Car)
     profile_pic = models.ImageField(upload_to = 'driversdp/',null=True)
     phone_number = models.IntegerField()
+
+
+    
--- riders/models.py
+++ riders/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+import datetime as dt
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Ridertag (models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Pickup_location(models.Model):
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+
+class Rider(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    pickup_point = models.ForeignKey(Pickup_location)
+    destination = models.CharField(max_length = 50)
+    tags = models.ManyToManyField(Ridertag)
+    profile_pic = models.ImageField(upload_to = 'driversdp/',null=True)
+    phone_number = models.IntegerField()
