1348b19e27225ead3a3c6f68f420f296adf7e8e3
mugala <mugalalevi21@gmail.com>
2018-05-20 15:55:17 +0300

    Pillow This commit will nstall pillow to the project for uploading profle pictures as well and updating the requirements.txt file

--- drivers/models.py
+++ drivers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+import datetime as dt
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Driver (models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    pickup_point = models.CharField(max_length = 50)
+    tags = models.ManyToManyField(tags)
+    profile_pic = models.ImageField(upload_to = 'driversdp/',null=True)
+    phone_number = models.IntegerField()
