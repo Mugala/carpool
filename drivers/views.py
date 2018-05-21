3051ea63b15d4fed190ff78ab18f1328164a76b4
mugala <mugalalevi21@gmail.com>
2018-05-21 23:34:38 +0300

    Forms and views This commit will create the forms and the view function for the drivers profile page

--- drivers/views.py
+++ drivers/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import  HttpResponse,Http404,HttpResponseRedirect
 from .models import Car,Pickup_location,Driver,NewsLetterRecipients
-from .forms import NewsLetterForm
+from .forms import NewsLetterForm,DriverProfile
 from .email import send_welcome_email
 from django.contrib.auth.decorators import login_required
 
@@ -28,6 +28,20 @@ def welcome(request):
     else:
         form = NewsLetterForm()     
     return render (request, "dwelcome.html",{"details":details, "letterForm":form})
+
+def user_profile(request):  
+    current_user = request.user
+    if request.method =='POST':
+        form = DriverProfile(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+
+            return redirect("home")
+    else:
+        form = DriverProfile()
+    return render (request, 'all-photos/edit_profile.html',{"form":form})
 
 
 def search_results(request):
